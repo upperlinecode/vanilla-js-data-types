5f098adac09af6a3bd36785fb6bab4bc3f038870
Ileini Melendez <122940019+ileini-melendez@users.noreply.github.com>
2023-03-16 13:33:27 +0000

    Initial commit

---
+++ READ_ME.md
@@ -0,0 +1,17 @@
+# JavaScript Lesson 1.1 Template - Greeting Program
+
+### Welcome! 
+
+Note: If you are coding along with the video, we suggest you open the video a window on the left and this repl.it template opened in a window on the right. This method of coding along is called "split screen".
+
+#### Using this template, you will be able to practice the following:
+
+- Printing statements to the JavaScript console.
+- Create variables that store various data types.
+- Use variables to print statements to the console.
+- Use JavaScript methods to format string data types.
+- Ask for user input and store their values in variables.
+
+#### Get Started:
+
+- On the left menu click on the file named "script.js" to get started.
---
+++ script.js
@@ -0,0 +1,39 @@
+// --------------------------------------------------------------
+// Hello Word --> code your first console.log statement below!
+
+
+// Mini Challenge --> OPTIONS: (1) Print out a five-sentence story with five different statements. (2)Google the phrase ASCII-art and try to make your progrma print it out to the console. Type your code below.
+
+
+// --------------------------------------------------------------
+// Strings & Creating Variables --> Code a variable that stores your greeting program user's name below.
+
+
+// Mini Challenge --> OPTIONS: (1) Change the string 'Eli' to another name. What prints to the console? (2)Create another variable 'hometown' and enter your hometown. Print the value of this variable in your console. Type your code below.
+
+
+// --------------------------------------------------------------
+// Numbers & Boolean Data Types --> Code a variable that stores your greeting program user's age below. 
+
+
+// Numbers & Boolean Data Types --> Code a variable that stores your greeting program user's boolean value of their location 'at_home' or 'at_work' below. 
+
+
+// --------------------------------------------------------------
+// Concatenating with Variables --> Code a console.log statement that prints your greeting program user's name along with a greeting below.
+
+
+// Mini Challenge --> Print another message to the console by concatenating two variables into one string. Type your code below.
+
+
+// --------------------------------------------------------------
+// JavaScript String Methods --> (1)Code a console.log statement that prints your greeting program user's name in capital letters below. (2) Code a console.log statement that prints the number of letters in your greeting program user's name below. 
+
+
+// --------------------------------------------------------------
+// User Input --> Use the keyword 'prompt' to code a variable that allows a user to input their name into your greeting program below. 
+
+
+// Mini Challenge --> Create another variable named 'fave_movie' that prompts the user to enter the name of their favorite movie below. Code a console.log statement that uses the variable below. 
+
+
